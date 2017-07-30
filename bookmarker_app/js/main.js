d528058611b37b1654a44d30cad9ddc0f43c9f01
tienpham94 <r3dsky02@yahoo.com.vn>
2017-07-30 23:51:56 +0300

    Added delete bookmarks functionality

--- bookmarker_app/js/main.js
+++ bookmarker_app/js/main.js
@@ -24,6 +24,16 @@ function saveBookmark(e){
   e.preventDefault();
 }
 
+function deleteBookmark(url){
+  var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+  for (var i = 0; i < bookmarks.length; i++) {
+    if(bookmarks[i].url == url){
+      bookmarks.splice(i,1);
+    }
+  }
+
+  localStorage.setItem("bookmarks",JSON.stringify(bookmarks));
+}
 
 function fetchBookmarks(){
   var bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
@@ -36,6 +46,10 @@ function fetchBookmarks(){
     var name = bookmarks[i].name;
     var url = bookmarks[i].url;
 
-    bookmarksResults.innerHTML += '<div class="well">' + '<h3>'+ name + '<a class="btn btn-default" target="_blank" href="'+url+'">Visit</a>' +'</h3>' + '</div>';
+    bookmarksResults.innerHTML += '<div class="well">' +
+    '<h3>'+ name +
+    '<a class="btn btn-default" target="_blank" href="'+url+'">Visit</a>' +
+    '<a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" target="_blank" href="'#'">Delete</a>' +
+    '</h3>' + '</div>';
   }
 }
